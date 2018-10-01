fe8173107e153fdae866cd6dcdc4d58f04a10b94
Unknown <teunvanlaarhoven@gmail.com>
2018-10-01 16:22:59 +0200

    Added testcase
    Added a testcase for the DamageAble class/script. This test uses the normal C# events/delegates instead of UnityEvents. This is preparation for the switch to normal events, because UnityEvents dont work well with networking.

---
+++ GD Wizard Bois/Assets/TestCases/Events/DamageTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DamageTest : NetworkBehaviour
+{
+	public int maxHealth = 10;
+	public int health;
+
+	public delegate void EventTakeDamageDelegate(int amount);
+
+	[SyncEvent] public event EventTakeDamageDelegate EventOnTakeDamage;
+
+	// Add event handler to the event
+	private void Start()
+	{
+		health = maxHealth;
+		EventOnTakeDamage += TakeTakeDamage;
+	}
+
+	// Call from outside
+	public void damage(int amount)
+	{
+		CmdTakeDamage(amount);
+	}
+
+	// Call to the server to invoke event
+	[Command]
+	private void CmdTakeDamage(int amount)
+	{
+		if (EventOnTakeDamage != null)
+			EventOnTakeDamage(amount);
+	}
+
+	// Event handler that applies the amount of damage
+	private void TakeTakeDamage(int amount)
+	{
+		health -= amount;
+	}
+}
