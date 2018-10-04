70450c6e537a9110e6512bb06d7aad99d15388cb
tom-x-s <34095193+tom-x-s@users.noreply.github.com>
2018-10-04 11:13:35 +0200

    Small progress
    Put almost everything in the right spot, starting to connect the scripts too.

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Slider[] healthBarSliders;
+    public Button[] AnyButton;
+    public float AlphaThreshold = 1f;
+
+    void Start()
+    {
+        AnyButton = GameObject.Find("Canvas").GetComponentsInChildren<Button>();
+        foreach (Button UIbutton in AnyButton)
+        {
+            UIbutton.onClick.AddListener(() => Debug.Log("AnyButtonPressed"));
+            UIbutton.GetComponent<Image>().alphaHitTestMinimumThreshold = AlphaThreshold;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InstructionControllerUI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InstructionController))]
+public class InstructionControllerUI : NetworkBehaviour
+{
+    public Image statusImage;
+    public Text timerText;
+    public Button nextGameButton;
+
+    private InstructionController instructionController;
+
+    private void Start()
+    {
+        instructionController = GetComponent<InstructionController>();
+
+        if (!isServer) nextGameButton.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        timerText.text = Math.Round(instructionController.timeLeft, 2).ToString();
+
+        if (instructionController.roundActive)
+        {
+            if (instructionController.sentReply)
+                statusImage.color = Color.yellow;
+            else
+                statusImage.color = Color.white;
+        }
+        else
+        {
+            if (instructionController.gameWon)
+                statusImage.color = Color.green;
+            else if (instructionController.gameLost)
+                statusImage.color = Color.red;
+        }
+    }
+
+    public void startNextMatch()
+    {
+        if (!isServer) return;
+        instructionController.RpcBeginNextRound();
+    }
+}
