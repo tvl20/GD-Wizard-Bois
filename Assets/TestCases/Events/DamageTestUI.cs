495940fcdc894625cdb213a59a27ae987699f4d6
tom-x-s <34095193+tom-x-s@users.noreply.github.com>
2018-10-04 12:49:57 +0200

    UI update again again
    Fourth batch of updates

---
+++ Assets/TestCases/Events/DamageTestUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageTestUI : MonoBehaviour
+{
+	public DamageTest healthScript;
+	public Text healthRemaining;
+	public Text healthChange;
+
+	void Start ()
+	{
+		healthScript.EventOnTakeDamage += OnTakeUpdateUi;
+		OnTakeUpdateUi(0);
+	}
+
+	private void OnTakeUpdateUi(int amount)
+	{
+		healthRemaining.text = healthScript.health.ToString();
+		healthChange.text = amount.ToString();
+	}
+}
