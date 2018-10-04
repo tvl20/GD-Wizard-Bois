9a485a000663417302830ef691b107e8cd6dc071
tom-x-s <34095193+tom-x-s@users.noreply.github.com>
2018-10-04 09:45:14 +0200

    Combining main game with ui
    Just threw it all together, let's hope it works.

---
+++ Assets/Scripts/InstructionController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class InstructionController : NetworkBehaviour
+{
+    public float timeLeft;
+
+    [SyncVar] public bool roundActive = false;
+
+    [SyncVar] public bool gameWon = false;
+    [SyncVar] public bool gameLost = false;
+
+    public bool sentReply = false;
+
+    private int numbPlayers;
+
+    private Dictionary<NetworkIdentity, TouchPatternInput.UniquePatterns> replies = new Dictionary<NetworkIdentity, TouchPatternInput.UniquePatterns>();
+
+    private void Start()
+    {
+        if (!isServer) return;
+
+        numbPlayers = NetworkManager.singleton.numPlayers;
+    }
+
+    private void Update()
+    {
+        if (roundActive)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft <= 0)
+            {
+                timeLeft = 0;
+//                Debug.Log("RESETTING TIMER");
+
+                if (!isServer) return;
+                roundActive = false;
+
+                gameWon = roundIsWon();
+                gameLost = !gameWon;
+            }
+        }
+
+
+        numbPlayers = NetworkManager.singleton.numPlayers;
+    }
+
+    public void addReply(NetworkIdentity playerID, TouchPatternInput.UniquePatterns patternReply)
+    {
+//        if (!roundActive || sentReply) return;
+        if (!roundActive) return;
+
+//        Debug.Log("from: " + playerID + " a thingy added to the replies: " + patternReply);
+        replies.Add(playerID, patternReply);
+        sentReply = true;
+    }
+
+    private bool roundIsWon()
+    {
+//        Debug.Log("checking victory condition");
+        HashSet<TouchPatternInput.UniquePatterns> patterns = new HashSet<TouchPatternInput.UniquePatterns>();
+
+        if (replies.Count != numbPlayers) return false;
+//        Debug.Log("all players submitted something");
+
+        foreach (KeyValuePair<NetworkIdentity,TouchPatternInput.UniquePatterns> pair in replies)
+        {
+            bool noDuplicate = patterns.Add(pair.Value);
+            if (!noDuplicate)
+            {
+                return false;
+            }
+        }
+//        Debug.Log("no duplicates found");
+
+        return true;
+    }
+
+    [ClientRpc]
+    public void RpcBeginNextRound()
+    {
+        roundActive = true;
+        sentReply = false;
+        timeLeft = 5;
+
+        if (!isServer) return;
+//        previousReplies = new Dictionary<NetworkIdentity, TouchPatternInput.UniquePatterns>(replies);
+        replies.Clear();
+
+    }
+}
---
+++ Assets/Scripts/TouchPatternInput.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PatternUnityEvent : UnityEvent<TouchPatternInput.UniquePatterns> { }
+
+public class IntUnityEvent : UnityEvent<int> { }
+
+public class TouchPatternInput : MonoBehaviour
+{
+	public PatternUnityEvent onFinishedPattern = new PatternUnityEvent();
+	public IntUnityEvent onNodeAddedToPattern = new IntUnityEvent();
+
+	public enum UniquePatterns
+	{
+		None,
+		VerticalLine,
+		HorizontalLine,
+		HighHorizontalLine
+	}
+
+	private class Pattern
+	{
+		public readonly UniquePatterns UniquePattern;
+		private readonly int[] PatternArray;
+
+		public Pattern(UniquePatterns uniquePattern, int[] patternArray)
+		{
+			UniquePattern = uniquePattern;
+			PatternArray = patternArray;
+		}
+
+		public bool PatternArrayMatches(int[] otherPatternArray)
+		{
+			if (otherPatternArray == null) return false;
+			return PatternArray.SequenceEqual(otherPatternArray);
+		}
+	}
+
+	private readonly List<Pattern> knownPatterns = new List<Pattern>()
+	{
+		{new Pattern(UniquePatterns.VerticalLine, new int[] {2, 5, 8})},
+		{new Pattern(UniquePatterns.HorizontalLine, new int[] {4, 5, 6})},
+		{new Pattern(UniquePatterns.HighHorizontalLine, new int[] {1, 2, 3})}
+	};
+
+	private List<int> currentPattern;
+	public List<int> getCurrentPattern()
+	{
+		//return a copy of the list
+		return new List<int>(currentPattern);
+	}
+
+	private bool finishedPatternFrame = false;
+
+	private void Start () {
+		currentPattern = new List<int>();
+	}
+
+	private void Update ()
+	{
+		if (finishedPatternFrame) finishedPatternFrame = false;
+		if (Input.touchCount <= 0) return;
+
+		if (Input.touches[0].phase == TouchPhase.Ended)
+		{
+			UniquePatterns pattern = checkPattern(currentPattern.ToArray());
+			Debug.Log(pattern);
+
+			string output = "";
+			foreach (int i in currentPattern)
+			{
+				output += i + ", ";
+			}
+			Debug.Log(output);
+
+			onFinishedPattern.Invoke(pattern);
+			currentPattern.Clear();
+			finishedPatternFrame = true;
+		}
+	}
+
+	public void addNodeToCurrentPattern(int node)
+	{
+		if (!currentPattern.Contains(node) && !finishedPatternFrame)
+		{
+			currentPattern.Add(node);
+			onNodeAddedToPattern.Invoke(node);
+//			Debug.Log(node + " , added to the current pattern");
+		}
+	}
+
+	private UniquePatterns checkPattern(int[] numberSequence)
+	{
+		foreach (Pattern knownPattern in knownPatterns)
+		{
+			if (knownPattern.PatternArrayMatches(numberSequence))
+			{
+				return knownPattern.UniquePattern;
+			}
+		}
+
+		return UniquePatterns.None;
+	}
+}
